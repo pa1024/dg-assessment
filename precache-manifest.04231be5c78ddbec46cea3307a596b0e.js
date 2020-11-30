2355ba8c620549bcce8d761723e0a3dd898efc7f
Pooja Ashoka <Pooja.Ashoka@ltts.com>
2020-12-01 01:04:09 +0530

    Updates

---
+++ precache-manifest.04231be5c78ddbec46cea3307a596b0e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d353a339333ff1070f31d06309eb1d",
+    "url": "/dg-assessment/index.html"
+  },
+  {
+    "revision": "035fe4a927471858303b",
+    "url": "/dg-assessment/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "448b5d1881b9c1908357",
+    "url": "/dg-assessment/static/css/main.f1a67d3b.chunk.css"
+  },
+  {
+    "revision": "035fe4a927471858303b",
+    "url": "/dg-assessment/static/js/2.1c5e4d62.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dg-assessment/static/js/2.1c5e4d62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448b5d1881b9c1908357",
+    "url": "/dg-assessment/static/js/main.4448e0a6.chunk.js"
+  },
+  {
+    "revision": "fd0975f22145799d1b46",
+    "url": "/dg-assessment/static/js/runtime-main.b84f5169.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/dg-assessment/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/dg-assessment/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/dg-assessment/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/dg-assessment/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/dg-assessment/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/dg-assessment/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/dg-assessment/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/dg-assessment/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/dg-assessment/static/media/poster9.65d6d500.jpg"
+  }
+]);
